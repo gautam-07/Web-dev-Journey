8e7ea5a55e4038d4ceefcb4034bebb88291e8b14
Gautam <gomzekh@gmail.com>
2021-01-29 14:59:33 +0530

    String variable practice

---
+++ Javascript/Strings/string1.js
@@ -0,0 +1,8 @@
+// String methods that doesn't take an argument.
+
+// Convert message into lower case and trim the extra spaces
+
+const message = "    TASTE THE RAINBOW!  ";
+
+let whisper = message.trim().toLowerCase();
+
