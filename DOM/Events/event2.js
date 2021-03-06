9778027b48e73bdc728d09d1d0fc9798c8f75662
Gautam <gomzekh@gmail.com>
2021-03-06 20:52:15 +0530

    addEventListner - DOM event

---
+++ DOM/Events/event2.js
@@ -0,0 +1,11 @@
+const btn1 = document.querySelector('#hello');
+
+const btn2 = document.querySelector('#goodbye');
+
+btn1.addEventListener('click', function () {
+   console.log("hello");
+})
+
+btn2.addEventListener('click', function () {
+   console.log("goodbye");
+})
