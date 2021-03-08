6417947de9a1a0963b4c1bd6c38e1425f24098ff
Gautam <gomzekh@gmail.com>
2021-03-08 18:58:04 +0530

    More of Random Colors

---
+++ DOM/Events/event4.js
@@ -0,0 +1,17 @@
+const randomColor = () => {
+   const r = Math.floor(Math.random() * 255);
+   const g = Math.floor(Math.random() * 255);
+   const b = Math.floor(Math.random() * 255);
+   return `rgb(${r}, ${g}, ${b})`
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+   button.addEventListener('click', colorize);
+}
+
+function colorize() {
+   this.style.backgroundColor = randomColor();
+   this.innerText = randomColor();
+}
