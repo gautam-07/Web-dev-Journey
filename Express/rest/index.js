34140619346fe6ee28ad4236441e896027fe8601
Gautam <gomzekh@gmail.com>
2021-03-25 20:00:22 +0530

    Index file

---
+++ Express/rest/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+
+
+
+app.listen(3000, () => {
+   console.log('Listening to port 3000');
+})
+
+
